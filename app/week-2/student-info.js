97109794c06ed304f41040d70195e5f5037ae7d2
J-Wales <151964658+J-Wales@users.noreply.github.com>
2024-05-27 09:12:07 -0600

    Format edit

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,12 +1,10 @@
 import Link from "next/link";
 
-function StudentInfo() {
+export default function StudentInfo() {
     return (
         <div>
-        <p>J Wales</p>
-        <Link href="https://github.com/J-Wales/cprg306-assignments">GitHub link</Link>
+            <p>J Wales</p>
+            <Link href="https://github.com/J-Wales/cprg306-assignments">GitHub link</Link>
         </div>
     );
 }
-
-export default StudentInfo;
