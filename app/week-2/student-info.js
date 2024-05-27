c1016941a2ae236e1b2415f745f995dd8bbef52e
J-Wales <151964658+J-Wales@users.noreply.github.com>
2024-05-27 08:28:10 -0600

    Created and modified week 2 project files

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+function StudentInfo() {
+    return (
+        <div>
+        <p>J Wales</p>
+        <Link href="https://github.com/J-Wales/cprg306-assignments">GitHub link</Link>
+        </div>
+    );
+}
+
+export default StudentInfo;
