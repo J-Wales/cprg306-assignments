7e30a55c178da71b33c485834ba316be76d78701
J-Wales <151964658+J-Wales@users.noreply.github.com>
2024-09-13 09:55:21 -0600

    Completed week 2 assignment
    Completed all needed elements for the week 2 assignment.

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+    return (
+        <div>
+            <p>J Wales</p>
+            <Link href="https://github.com/J-Wales/cprg306-assignments.git">Repository Link</Link>
+        </div>
+    );
+}
